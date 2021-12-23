ccd5c85f867bf817b4954d6f449c29fb2f2b0c91
cch76 <cch.keti@gmail.com>
2021-12-23 13:13:23 +0900

    Feat: redis DB install file(#10)
    redis DB 설치 sh 파일 추가 및 DB 추가에 따른 server, client py파일 수정

--- client.py
+++ client.py
@@ -10,18 +10,17 @@ def get_params(height, weight):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Toy clinet")
+    parser.add_argument("--name", default="cch", type=str)
     parser.add_argument("--height", default=170, type=int)
     parser.add_argument("--weight", default=70, type=int)
     args = parser.parse_args()
 
     url = "http://10.0.7.57:5000/"
 
-    home = requests.get(url)
-    print(home.text)
+    test = requests.get(
+        f"{url}test/?name={args.name}&height={args.height}&weight={args.weight}"
+    )
+    print(test.text)
 
-    test = requests.get(url + "test/")
-    print(test.text[13:-2])
-
-    params = get_params(args.height, args.weight)
-    res = requests.get(url + "bmi/", params=params)
-    print(res.text)
+    judgement = requests.get(f"{url}judgement/?name={args.name}")
+    print(f"your info \n{judgement.text}")
--- server.py
+++ server.py
@@ -1,57 +1,88 @@
 from flask import Flask, request
 import requests
 import json
+import redis
 
 app = Flask(__name__)
+r = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+
+def cal_bmi(height, weight):
+    return weight / (height / 100 * height / 100)
 
 
 @app.route("/")
 def root():
-    return "Welcome!\n"
+    return "Welcome!"
 
 
-@app.route("/test/")
-def func1():
-    text = {"message": "Hello, Tester!"}
-    return json.dumps(text)
+@app.route("/test/", methods=["GET"])
+def name():
+    name = request.args.get("name", "cch")
+    height = int(request.args.get("height", "173"))
+    weight = int(request.args.get("weight", "80"))
+
+    text = f"Hello, {name}!"
+
+    params = {
+        "name": name,
+        "height": height,
+        "weight": weight,
+    }
+
+    requests.post("http://127.0.0.1:5000/bmi/", data=json.dumps(params))
+
+    return text
 
 
-@app.route("/bmi/", methods=["GET"])
-def cal_bmi():
-    height = int(request.args.get("height", "170"))
-    weight = int(request.args.get("weight", "60"))
+@app.route("/bmi/", methods=["POST"])
+def bmi():
+    params = json.loads(request.get_data())
+
+    name = params["name"]
+    height = params["height"]
+    weight = params["weight"]
+    bmi = cal_bmi(params["height"], params["weight"])
 
     params = {
         "height": height,
         "weight": weight,
-        "bmi": weight / (height / 100 * height / 100),
+        "bmi": bmi,
     }
 
-    res = requests.post("http://127.0.0.1:5000/judgement/", data=json.dumps(params))
+    json_params = json.dumps(params)
 
-    return res.text
+    r.set(name, json_params)
+
+    return json.dumps(params)
 
 
-@app.route("/judgement/", methods=["POST"])
+@app.route("/judgement/", methods=["GET"])
 def judge_weight():
-    params = json.loads(request.get_data())
+    name = request.args.get("name", "cch")
 
-    if len(params) == 0:
+    params = r.get(name)
+    json_params = json.loads(params)
+
+    # check None
+    if len(json_params) == 0:
         return "No parameter!"
 
-    result = {"bmi": params["bmi"], "category": "temp"}
+    bmi = json_params["bmi"]
 
-    if params["bmi"] < 20:
-        result["category"] = "Underweight"
+    if bmi < 20:
+        category = "Underweight"
 
-    if params["bmi"] >= 20 and params["bmi"] < 25:
-        result["category"] = "Normal weight"
+    elif bmi >= 20 and bmi < 25:
+        category = "Normal weight"
 
-    if params["bmi"] >= 25 and params["bmi"] < 30:
-        result["category"] = "Overweight"
+    elif bmi >= 25 and bmi < 30:
+        category = "Overweight"
 
-    if params["bmi"] >= 30:
-        result["category"] = "Obesity"
+    elif bmi >= 30:
+        category = "Obesity"
+
+    result = {"name": name, "bmi": bmi, "category": category}
 
     return json.dumps(result)
 
